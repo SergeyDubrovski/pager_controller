e79df3588d06dbcc784923430b4a7d978b09d84a
SergeyDubrovski <sovelectron@gmail.com>
2024-02-11 08:11:59 +0300

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,4 @@
 /*
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-#include <AsyncElegantOTA.h>
-#include <LiquidCrystal_I2C.h>
-#include <ArduinoWebsockets.h>
-
-const char *ssid = "UltraLab_Work";
-const char *password = "Minsk@Ultra";
-const char *websockets_server_host = "91.215.176.34"; // Enter server adress
-const uint16_t websockets_server_port = 7012;       // Enter server port
-
-using namespace websockets;
-
-AsyncWebServer server(80);
-WebsocketsClient client;
-String message2 = "Hello1";
-String newMess = "";
-
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-void onMessageCallback(WebsocketsMessage message)
-{
-  lcd.clear();
-  if (message.length() > 0)
-  {
-    lcd.backlight();
-    newMess = message.data();
-
-    lcd.setCursor(0, 0);
-    lcd.print(newMess.substring(0, 17));
-    lcd.setCursor(0, 1);
-    lcd.print(newMess.substring(17));
-
-  } else {
-    lcd.clear();
-    lcd.noBacklight();
-  }
-}
-
-void setup(void)
-{
-  IPAddress ip(10, 10, 10, 248);
-  IPAddress subnet(255, 255, 255, 0);
-  IPAddress gateway(10, 10, 10, 1);
-
-  Serial.begin(9600);
-  //WiFi.config(ip, gateway, subnet);
-   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
-  // LiquidCrystal_I2C lcd(0x27, 16, 2);
-
-  lcd.init();
-
-  lcd.setCursor(0, 1);
-  lcd.print("Hello, world");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(200, "text/plain", "Hi! I am ESP8266."); });
-
-  AsyncElegantOTA.begin(&server); // Start ElegantOTA
-  server.begin();
-  Serial.println("HTTP server started");
-
-  client.connect(websockets_server_host, websockets_server_port, "/api/esp");
-
-  client.onMessage(onMessageCallback);
-}
-
-void loop(void)
-{
-  client.poll();
-
- client.connect(websockets_server_host, websockets_server_port, "/api/esp");
-
-  client.send("Hello Server" + String(2));
-
-  delay(1000);
-}*/
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
@@ -144,19 +52,7 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
     }
 
     break;
-    /*
-  case WStype_BIN:
-    DEBUG_SERIAL.printf("[WSc] get binary length: %u\n", length);
-    hexdump(payload, length);
-    break;
-  case WStype_PING:
-    // pong will be send automatically
-    DEBUG_SERIAL.printf("[WSc] get ping\n");
-    break;
-  case WStype_PONG:
-    // answer to a ping we send
-    DEBUG_SERIAL.printf("[WSc] get pong\n");
-    break;*/
+
   }
 }
 
@@ -236,4 +132,78 @@ void loop()
 
     // lastUpdate = millis();
   }
+}*/
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <LiquidCrystal_I2C.h>
+#include "ESP8266WebServer.h"
+
+const char *ssid = "TP-Link_5306"; // UltraLab
+const char *password = "Serg1990"; // Minsk@Ultra
+
+unsigned long messageInterval = 5000;
+bool connected = false;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+ESP8266WebServer server(81);
+String message = "";
+
+void standartMessage()
+{
+  lcd.clear();
+  lcd.noBacklight();
+  lcd.setCursor(0, 0);
+  lcd.print("Im ready for mess.");
+  lcd.setCursor(0, 1);
+  lcd.print(WiFi.localIP());
+}
+void handleSpecificArg()
+{
+
+  message += server.arg("message");
+  server.send(200, "text/plain", message);
+  if (message.length() > 0)
+  {
+    lcd.clear();
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print(message.substring(0, 16));
+    lcd.setCursor(0, 1);
+    lcd.print(message.substring(16));
+    message = "";
+    delay(30000);
+    standartMessage();
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+
+  IPAddress local_IP(192, 168, 1, 106);
+
+  IPAddress gateway(192, 168, 1, 1);
+
+  IPAddress subnet(255, 255, 255, 0);
+  IPAddress primaryDNS(192, 168, 1, 1); // optional
+  IPAddress secondaryDNS(8, 8, 8, 8);   // optional
+
+  WiFi.begin(ssid, password);
+  WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  server.on("/", handleSpecificArg);
+  server.begin();
+  lcd.init();
+  standartMessage();
+}
+
+// unsigned long lastUpdate = millis();
+
+void loop()
+{
+  server.handleClient();
 }
